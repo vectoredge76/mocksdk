8b1b1ba1504b2006d149999748245f2436e5e8c0
Manav Gurnani <168049366+manavgurnani@users.noreply.github.com>
2024-06-26 15:48:28 -0700

    adding header file to aho-corasick for GTEST

---
+++ docker_setup/alg_project/include/aho_corasick.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <string>
+
+const int MAX_CHARS;
+
+struct TrieNode {
+	public:
+		TrieNode* children; // 26 possible states for each red
+		TrieNode* parent; // the parent of each state and where it came from
+		TrieNode* fail; // the pointer to a failure link from every node
+		std::vector<int> output; // output for the end of words on the node
+		char value;
+		TrieNode(char ch);
+};
